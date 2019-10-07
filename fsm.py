15e3d3137ebf9217bf5da2869cd78ad2eda32576
mathildehaugum <mathilhh@stud.ntnu.no>
2019-10-07 13:54:19 +0200

    fixed import

--- fsm.py
+++ fsm.py
@@ -1,6 +1,6 @@
 """Class for making an FSM object with associated methods"""
-import Gruppeprosjekt.plab.rule as rule
-import Gruppeprosjekt.plab.kpc_agent as kpc_agent
+import rule
+import kpc_agent
 
 
 class FSM:
@@ -9,24 +9,35 @@ class FSM:
         self._rule_list = []
 
     def add_rule(self, rule):
+        """add a new rule-object to the end of rule_list"""
         self._rule_list.add(rule)
 
     def get_next_signal(self):
-        return
+        """gets next_signal from agent"""
+        kpc_agent.get_signal()
 
     def run_rules(self):
+        """apply each rule in rule_list until one is fired"""
         for rule in self._rule_list:
             if self.apply_rule(rule):
                 self.fire_rule(rule)
 
     def apply_rule(self):
+        """check if conditions of rule are met"""
         return
 
     def fire_rule(self):
+        """"depending on the rule will (1) set the next state and (2) call agent action method"""
         return
 
     def main_loop(self):
-        return
+        """begins in initial state and call get_next_signal og run_rules until FSM enters final state"""
+        state =
+
+
+
+        while state !=
+
 
 
 

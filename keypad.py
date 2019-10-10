a67d4cdd8013f3fac75a4c1b75f68c067ca89ca4
mathildehaugum <mathilhh@stud.ntnu.no>
2019-10-10 19:15:08 +0200

    fix

--- keypad.py
+++ keypad.py
@@ -46,6 +46,7 @@ class KeyPad:
         key = None
         while key == None:
             key = self.do_polling()
+            time.sleep(0.7)
         print("key: " + str(key))
         print("reached keypad after loop")
         return key
